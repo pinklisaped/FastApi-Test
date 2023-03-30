5ebc3e3b62f1161f44b651ba51fb9783db30d172
Sergey Rozhin <namesergeyrozhin@gmail.com>
2023-03-31 00:48:32 +0300

    Split exit and throw

--- main.py
+++ main.py
@@ -1,13 +1,16 @@
 import random
 import string
 import os
+from typing import Optional
 
 from fastapi import FastAPI, HTTPException
 from starlette import status
 from starlette.responses import Response
 
+
 app = FastAPI(title="Debug app")
 id = ''.join(random.choice(string.ascii_lowercase + string.ascii_uppercase + string.digits) for i in range(20))
+
 
 @app.get("/app/get_id")
 async def get_id():
@@ -17,13 +20,27 @@ async def get_id():
 
 @app.get("/app/throw_exception")
 async def throw_exception():
-    '''Always app exit'''
-    os._exit(1)
+    '''Always app throw exception'''
+    raise SystemExit('Some exception')
 
 
 @app.get("/app/throw_exception/{app_id}")
 async def throw_exception_arg(app_id):
-    '''Selected app exit'''
+    '''Selected app throw exception'''
     if id == app_id:
         await throw_exception()
     return HTTPException(status_code=status.HTTP_404_NOT_FOUND)
+
+
+@app.get("/app/exit/{app_id}")
+    '''Selected app exit'''
+async def exit_app_id(app_id, exitcode: Optional[int] = 1):
+    if id == app_id:
+        await exit_app(exitcode)
+    return HTTPException(status_code=status.HTTP_404_NOT_FOUND)
+
+
+@app.get("/app/exit")
+async def exit_app(exitcode: Optional[int] = 1):
+    '''Always app exit'''
+    os._exit(exitcode)
